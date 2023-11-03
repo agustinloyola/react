374b724d23d23acbd765302952305e1c89059e3b
Agustin Loyola <loyolagustin@gmail.com>
2023-11-02 22:35:21 -0300

    segundo push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBarComponent from './components/NavBar/NavBarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <NavBarComponent/>
+    </div>
+  );
+}
+
+export default App;
